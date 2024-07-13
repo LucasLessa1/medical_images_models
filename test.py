17ca6ba07058bdd4f0bee3a0359e46c769fc160f
Lucas Lessa <79370622+LucasLessa1@users.noreply.github.com>
2024-07-13 15:36:41 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 import zipfile
 import os
 
-
-
-def extract_zip(zip_path, extract_to='.'):
+def extract_zip(zip_path, extract_to):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
 
 dataset_zip = '/mnt/nas/LucasLessa/COVID_Dataset_original.zip'
+extract_to =  '/mnt/nas/LucasLessa/medical_images_models/COVID_Dataset_original'
 
 print(dataset_zip)
-extract_zip(dataset_zip)
+extract_zip(dataset_zip, extract_to)
