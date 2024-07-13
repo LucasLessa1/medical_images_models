8a751a80ed5f0405f052d3add868510daca21626
Lucas Lessa <79370622+LucasLessa1@users.noreply.github.com>
2024-07-13 18:39:15 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
+import gdown
 import zipfile
 import os
 
-def extract_zip(zip_path, extract_to):
+def download(id):
+  url = 'https://drive.google.com/uc?id=' + str(id)
+  gdown.download(url, output = None, quiet = False)
+
+def extract_zip(zip_path, extract_to='.'):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
 
-dataset_zip = '/mnt/nas/LucasLessa/COVID_Dataset_original.zip'
-extract_to =  '/mnt/nas/LucasLessa/medical_images_models/COVID_Dataset_original'
+## Dataset
+# https://drive.google.com/file/d/12486kfJmFGLrVzMGGVM4kOIBOjwG5BOS/view?usp=sharing
 
-print(dataset_zip)
-extract_zip(dataset_zip, extract_to)
+## modules
+# https://drive.google.com/file/d/1wp9rTiYwX3dJgbwdvifc86_CsC37q4hj/view?usp=sharing
+# Download files
+download('12486kfJmFGLrVzMGGVM4kOIBOjwG5BOS')
+download('1wp9rTiYwX3dJgbwdvifc86_CsC37q4hj')
+
+
+dataset_zip = 'COVID_Dataset_original.zip'
+modules_zip = 'medical_images_models.zip'
+
+# Extract files
+extract_zip(dataset_zip)
+extract_zip(modules_zip)
