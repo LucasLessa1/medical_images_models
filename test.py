c3134935f7d372cb8dc4c21d1295bf3281c33677
Lucas Lessa <79370622+LucasLessa1@users.noreply.github.com>
2024-07-13 15:24:43 -0300

    Update test.py

--- test.py
+++ test.py
@@ -7,15 +7,7 @@ def extract_zip(zip_path, extract_to='.'):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to)
 
-## Dataset
-# https://drive.google.com/file/d/12486kfJmFGLrVzMGGVM4kOIBOjwG5BOS/view?usp=sharing
-
-## modules
-# https://drive.google.com/file/d/1wp9rTiYwX3dJgbwdvifc86_CsC37q4hj/view?usp=sharing
-# Download files
-
 dataset_zip = '/mnt/nas/LucasLessa/COVID_Dataset_original.zip'
 
 print(dataset_zip)
-# Extract files
 extract_zip(dataset_zip)
